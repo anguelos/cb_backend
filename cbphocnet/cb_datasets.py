e63e9a7f652cba4a408ec16c8bbf73282120ea31
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2021-04-27 03:18:06 +0200

    cropping phocnet inputs

--- cbphocnet/cb_datasets.py
+++ cbphocnet/cb_datasets.py
@@ -45,7 +45,7 @@ class CBDataset(object):
                     elif fixed_size is not None and pad_mode=="padcropscale":
                         in_w, in_h = word.size
                         out_w, out_h = fixed_size
-                        if in_w/out_w > 1 or in_h/out_w > 1:
+                        if in_w/out_w > 1 or in_h/out_h > 1:
                             ratio = min(out_h/in_h, out_w/in_w)
                             word = word.resize((int(word.size[0]*ratio), int(word.size[1]*ratio)))
                         in_w, in_h = word.size
