935403a04b44c8a838bcfdb8072ec9857c739e96
Anguelos Nicolaou <anguelos.nicolaou@gmail.com>
2021-06-03 17:54:52 +0200

    evaluate service more metrics

--- cbphocnet/__init__.py
+++ cbphocnet/__init__.py
@@ -1,5 +1,5 @@
 from .datasets import GWDataset
-from .cb_datasets import CBDataset
+from .cb_datasets import CBDataset, resize_word
 from .util import my_torch_load, my_torch_save, map_from_query_test_feature_matrices, map_from_feature_matrix, \
     HomographyAugmentation, Logger, Statistics, LineListIO, check_size, average_precision, AverageMeter
 from .phoc import build_correlated_phoc, build_phoc_descriptor, get_unigrams_from_strings, get_n_grams, \
